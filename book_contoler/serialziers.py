58a74599a4072e2fdbb1e252f66a7836c87ec44b
cyuber.devoloper <sabdumajid91@gmail.com>
2025-11-22 19:21:05 +0500

    first commit

--- book_contoler/serialziers.py
+++ book_contoler/serialziers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ["book_name" ,"title","author","pages","pice","have"]
+        fields = ["book_namegit" ,"title","author","pages","pice","have"]
